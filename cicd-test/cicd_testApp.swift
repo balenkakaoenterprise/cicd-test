94c0533f38e490c40ed505260bbef7df276a4c9c
balenkakaoenterprise <101wp719@gmail.com>
2024-11-01 16:09:02 +0900

    Initial Commit

---
+++ cicd-test/cicd_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicd_testApp.swift
+//  cicd-test
+//
+//  Created by 이승호 on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct cicd_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cicd-testTests/cicd_testTests.swift
@@ -0,0 +1,16 @@
+//
+//  cicd_testTests.swift
+//  cicd-testTests
+//
+//  Created by 이승호 on 11/1/24.
+//
+
+import Testing
+
+struct cicd_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
